f34c50934be95d10eb2ecec97d2b0b5484a66b6c
donwee76 <140222460+donwee76@users.noreply.github.com>
2024-03-25 17:13:44 +0800

    Initial Commit

---
+++ backend/main.py
@@ -0,0 +1,12 @@
+#main.py
+
+from fastapi import FastAPI
+from core.config import settings
+
+app = FastAPI(title=settings.PROJECT_NAME,version=settings.PROJECT_VERSION)
+
+@app.get("/")
+def hello_api():
+    return {"msg":"Hello FastAPI🚀"}
+
+#To start the uvicorn server, type uvicorn main:app --reload
